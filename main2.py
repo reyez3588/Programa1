dca344dcdfce62546a99ed6edd2aa4248a8bd1ce
reyez3588 <reyez3588@gmail.com>
2021-11-03 09:35:07 -0600

    Este es el tercer commit

--- main2.py
+++ main2.py
@@ -19,4 +19,4 @@ with open('calificaciones.csv') as csvfile:
 for alumno in listAlumnos:
     print("Nombre:", alumno.getNombre() + ",", "Promedio:", alumno.getPromedio())
 
-print("Se supone que esto debe estar en linea y un segundo")
+print("Se supone que esto debe estar en linea y un segundo y se pueden poner los que quieran")
